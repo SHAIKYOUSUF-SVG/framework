601d14903057040088d0b1dfc634941b6549ac36
SHAIKYOUSUF-SVG <skyousuf9989@gmail.com>
2024-08-24 11:55:09 +0530

    Update ascending.py

--- ascending.py
+++ ascending.py
@@ -2,7 +2,15 @@
 
 
 def asc_list(list1):
+
+
+
+
+ 
  for i in range (0,len(list1)):
+
+
+  
    for j in range(i+1,len(list1)):
       if list1[i] > list1[j]:
          list1[i],list1[j] = list1[j],list1[i]
