5be64432d97fc9e1892f5dfe543897968ea127b9
srinu <srinuchowdhari142@gmail.com>
2024-08-26 07:03:59 +0000

    range.py pushing

---
+++ range.py
@@ -0,0 +1,43 @@
+n=9
+sum = 2 ;
+for i in range (n) :
+	sum=sum+i
+	print (sum)
+	sum = sum//2
+#print('given value are aceptable')
+    print (sum)
+
+    #for.py
+    for i in range (5) :
+ print (i)
+#while.py
+avalue= int(input('Please enter the number 10: '))
+while (avalue != 10):
+    print('Your input value is not equal to 10')
+    print('Please try again: ')
+    avalue= int(input('Enter the number 10: '))
+print('Thank you')
+print('You entered a value of 10')
+#elif.py
+value = int('3')
+if value>=10:
+	value=value/2
+	print(value)
+	print('given condition is true')
+elif (value<10) and (value>4):
+	value= value/5
+	print (value)
+else:
+    value=value*100
+    print (value)
+    print ('this is the value')
+#ifelse
+value =int('5')
+if value>10:
+    value=value/2
+    print ()
+else:
+    value=value//5
+    print (value)
+    print ('given values are execcuted')
+
